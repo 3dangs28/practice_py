8cf20cec1a0fa800375a2a66c8c63c17a06aaeb9
3dangs28 <3dangs28@gmail.com>
2022-05-31 03:11:53 -0500

    inicio de practicas hasta el punto 20

---
+++ index.py
@@ -0,0 +1,29 @@
+# acá va un comentario
+#if 3 > 5:
+    #print('Esto no se va a imprimir')
+#else:
+    #print('lo que es')
+a, b, c = 'lala', 'lele', 'lili'
+palabra = 'palabra'
+oracion = 'esto es una oracion'
+#print(a,b,c)
+valor1 = valor2 = valor3 = 'puerco feliz'
+#print(valor1, valor2, valor3)
+inicio= 'hola'
+final= 'mundo'
+#print(inicio + final) #concatenacion
+
+entero = 4 # integer
+conDecimales = 20.2  # float
+#definición números complejos
+complejo = 1j
+#print(palabra, oracion, entero, conDecimales, complejo)
+lista = [1,2,3]
+lista2 = lista.copy()
+lista.append(4) #agrega en la lista
+#lista.clear() #vacia la lista
+#lista.count(contar el valor que se quiere buscar)
+
+print(lista, lista2.count(1))
+print(len(lista)) # len() se utiliza para contar elementos
+
